8d08d9ad9d26ff211b832a661feed519f534d286
artem-kochetkov-94 <artem-kochetkov-94@mail.ru>
2023-05-15 22:23:32 +0400

    feature/9-weather-api

---
+++ src/exercises/9-weather-api/index.ts
@@ -0,0 +1,28 @@
+import express from "express";
+import { isAxiosError } from "axios";
+import { getWeather } from "./services/api.service";
+
+const port = 8005;
+const app = express();
+
+app.get("/weather", async (req, res) => {
+  if (!req.query.city || typeof req.query.city !== "string") {
+    return res.status(400).send("Bad request");
+  }
+
+  try {
+    const weather = await getWeather(req.query.city);
+    res.send(weather);
+  } catch (e) {
+    if (isAxiosError(e) && e.response?.status) {
+      res.status(e.response?.status).send(e.response?.statusText);
+      return;
+    }
+
+    res.status(500);
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Сервер запущен на https://localhost:${port}`);
+});
